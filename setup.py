c64dff34c2e14ce79e18b5913e57492d526ac05c
Jerin Peter George <jerinpetergeorge@scrapehero.com>
2018-08-11 00:28:12 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='django-database-functions',
+      version='0.1',
+      description='Implementation of few database for Django',
+      url='http://github.com/storborg/funniest',
+      author='Jerin Peter GeorgeFlying Circus',
+      author_email='jerinpetergeorge@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
